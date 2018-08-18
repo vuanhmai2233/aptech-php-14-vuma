ad4c1597bcc38846fc38b04b561948565f0a3de1
MaiAnhVu <vuanhmai23@gmail.com>
2018-08-18 16:17:00 +0700

    created 2018-8-18

---
+++ 2018-8-18/script.js
@@ -0,0 +1,9 @@
+
+function changeImage() {
+    var image = document.getElementById('myImage');
+    if (image.src.match("bulbon")) {
+        image.src = "pic_bulboff.gif";
+    } else {
+        image.src = "pic_bulbon.gif";
+    }
+}
