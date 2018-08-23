8fb36a373be33fbe23ac3ad91dd807734f0b1178
MaiAnhVu <vuanhmai23@gmail.com>
2018-08-23 16:47:36 +0700

    created 2018-8-23

---
+++ 2018-8-23/script.js
@@ -0,0 +1,25 @@
+// function anHien() {
+//     var bg = document.getElementById('js-bg')
+//     console.log('hello')
+//     if(bg.style.display==='none'){
+//         bg.classList.add("styleAnHien");
+//         bg.style.display='block';
+//     }
+//     else{
+//         bg.classList.remove("styleAnHien")
+//         bg.style.display='none'
+//     }
+// }
+function anHien() {
+        var bg = document.getElementById('js-bg')
+        // bg.className='d-block'
+        if(bg.classList.contains('d-none')){
+            bg.classList.remove('d-none');
+            bg.classList.add('d-block');
+
+        }else{
+            bg.classList.remove('d-block');
+            bg.classList.add('d-none');
+        }
+    }
+    
